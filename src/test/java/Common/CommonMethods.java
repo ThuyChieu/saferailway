0d73f23c0004a993968f052fe38f53fb15050409
ThuyChieu <chieuthuymaijenny@gmail.com>
2023-04-08 17:00:34 +0700

    update project

---
+++ src/test/java/Common/CommonMethods.java
@@ -0,0 +1,32 @@
+package Common;
+
+import PageObjects.Railway.LoginPage;
+import PageObjects.Railway.RegisterPage;
+import Utilities.TestReporter;
+import Utilities.Utility;
+import com.aventstack.extentreports.ExtentTest;
+import org.openqa.selenium.support.PageFactory;
+
+import static Utilities.Utility.log4j;
+
+
+public class CommonMethods {
+
+    public static void registerAndGoToLoginPage(ExtentTest logStep, String emailAddress, String password, String PID) {
+        try {
+            log4j.info("registerAndGoToLoginPage - Starts");
+            TestReporter.logInfo(logStep, "registerAndGoToLoginPage ...");
+
+            RegisterPage registerPage = PageFactory.initElements(Utility.getDriver(), RegisterPage.class);
+            registerPage.navigateRegisterPage();
+            registerPage.inputInformation(emailAddress, password, PID);
+            registerPage.clickBtnRegister();
+            log4j.info("registerAndGoToLoginPage method - Ends");
+        } catch (Exception e) {
+            log4j.error("registerAndGoToLoginPage method - ERROR: ", e);
+            TestReporter.logException(logStep, "registerAndGoToLoginPage - ERROR", e);
+        }
+    }
+
+
+}
